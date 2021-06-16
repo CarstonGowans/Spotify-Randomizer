ae5414eb4d65b59f724d15f55e97d5829cfa7eca
Carston Gowans <shockercj@gmail.com>
2021-06-15 19:54:27 -0600

    Incoporoated package structure and added package information for files

---
+++ com/github/carstongowans/cs3230/Cli.java
@@ -0,0 +1,75 @@
+        package com.github.carstongowans.cs3230;
+
+        import java.util.ArrayList;
+        import java.util.Scanner;
+        
+        class Cli {
+        public static void main(String [] args) {                                   // Main Driver code for Cli
+        //Cli cli = new Cli();
+        System.out.println("Enter list of Integers. (space separated): ");
+        int [] ints = MathCalculations.stringArrayToIntArray(MathCalculations.getUserInput().split("\\s+"));  // Input Information into Array from stringToIntArray() function
+       
+        int operation = 0;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Choose an Operation\n1. Sum - sum the integers\n2. Evens - find the evens\n3. Odds - find the odds\n4. Max - find the max\n5. Min - find the min\n6. Update - enter a new list of integers\n7. Exit");
+        
+        while(operation != 7) {                                                 // While loop to loop through commands
+            operation = scan.nextInt();
+            switch(operation) {
+                case 1: {                                                       // Output for Sum
+                    int sum = MathCalculations.sum(ints);
+                    System.out.println("Sum - " + sum);
+                    break;
+                }
+                case 2: {                                                       // Output for Evens
+                    ArrayList<Integer> evens = MathCalculations.evens(ints);
+                    System.out.print("Even: ");
+                    for(int i = 0; i < evens.size(); i++) {
+                        if(i != evens.size() - 1) {
+                            System.out.print(evens.get(i) + ", ");
+                        }
+                        else {
+                            System.out.println(evens.get(i));
+                        }
+                    }
+                    break;
+                }
+                case 3: {                                                       // Output for Odds                          
+                    ArrayList<Integer> odds = MathCalculations.odds(ints);
+                    System.out.print("Odds: ");
+                    for(int i = 0; i < odds.size(); i++) {
+                        if(i != odds.size() - 1) {
+                            System.out.print(odds.get(i) + ", ");
+                        }
+                        else {
+                            System.out.println(odds.get(i));
+                        }
+                    }
+                    break;
+                }
+                case 4: {                                                       // Output for Max Value
+                    int max = MathCalculations.max(ints);
+                    System.out.println("Max: " + max);
+                    break;
+                }
+                case 5: {
+                    int min = MathCalculations.min(ints);                                    // Output for Min Value
+                    System.out.println("Min: " + min);
+                    break;
+                }
+                case 6: {                                                       // Funciton call for inserting new Ints
+                    System.out.println("Enter list of Integers. (space separated): ");
+                    ints = MathCalculations.stringArrayToIntArray(MathCalculations.getUserInput().split("\\s+"));
+                    System.out.println("Choose an Operation\n1. Sum - sum the integers\n2. Evens - find the evens\n3. Odds - find the odds\n4. Max - find the max\n5. Min - find the min\n6. Update - enter a new list of integers\n7. Exit");
+                    break;
+                }
+                case 7:                                                         // Function call to break out of loop
+                    operation = 7;
+                    break;
+            }
+        }
+        System.out.println("Goodbye!");
+        scan.close();
+    }
+
+}
---
+++ com/github/carstongowans/cs3230/MathCalculations.java
@@ -0,0 +1,84 @@
+package com.github.carstongowans.cs3230;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MathCalculations {
+    public static Scanner myScanner = new Scanner(System.in);      // Scanner For Input            
+
+    public static String getUserInput() {                          // Function for gathering User Input
+        System.out.print("> ");
+        return myScanner.nextLine();
+    }
+
+    public static int[] stringArrayToIntArray(String[] strings) {  // Function for converting String Input to Int array
+        int[] integers = new int[strings.length];               
+
+            for(int i = 0; i < strings.length; i++) {
+                try {
+                    integers[i] = Integer.parseInt(strings[i]);
+                } catch (NumberFormatException ex) {
+                    System.out.println("invalid integer, try again");
+                }
+            }
+        return integers;
+    }
+
+    public static int sum(int[] intArray) {                     // Function for calculating sum
+        return Arrays.stream(intArray).sum();                   // Stream function to parse the sum of the total array. 
+                                                                // Streams can be used to parse the total array for it's information. So in this case, we're grabbing
+                                                                // the information from the array through the stream and throwing that information into the .sum() function
+    }
+
+    public static ArrayList<Integer> evens(int[] intArray) {    // Function for calculating even values
+        ArrayList<Integer> evens = new ArrayList<Integer>();
+        for(int i = 0; i < intArray.length; i++) {
+            if(intArray[i] % 2 == 0) {                          // Determine if int is even using Modulo Division
+                evens.add(intArray[i]);                
+            }
+        }
+        return evens;
+    }
+
+    public static ArrayList<Integer> odds(int[] intArray) {     // Function for determining odd values
+        ArrayList<Integer> odds = new ArrayList<Integer>();
+        for(int i = 1; i < intArray.length; i++) {
+            if(intArray[i] % 2 != 0) {                          // Determine if int is odd using Modulo Division
+                odds.add(intArray[i]);                
+            }
+            else {}
+        }
+        return odds;
+    }
+
+    public static int max(int[] intArray) {                     // Function for calculating new max value
+        if (intArray.length == 1) {                             // Return first number if size is 1
+            return intArray[0];
+        }
+        else {
+        int max = Integer.MIN_VALUE;
+        for(int i = 1; i < intArray.length; i++) {              // Else, parse through the array to determine the max value
+            if(intArray[i] > max) {                             
+                max = intArray[i];
+            }
+        }
+        return max;
+        }
+    }
+    
+    public static int min(int[] intArray) {                     // Function for calculating new min min value
+        if (intArray.length == 1) {                             // Return first number if size is 1
+            return intArray[0];                 
+        }
+        else {
+        int min = Integer.MAX_VALUE;
+        for(int i = 1; i < intArray.length; i++) {              // Else, parse through the array to determine the min value
+            if(intArray[i] < min) {                    
+                min = intArray[i];
+            }   
+        }
+        return min;
+        }
+    }
+}
